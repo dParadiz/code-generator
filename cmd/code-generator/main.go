75cd030f5907ba10d6675031475d22333a88052c
DParadiz <dpradiz@gmail.com>
2020-05-31 16:54:38 +0200

    Making flags work

--- cmd/code-generator/main.go
+++ cmd/code-generator/main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/dparadiz/code-generator/internal/generator"
 )
 
 func main() {
 
-	outputFolder := flag.String("output", "", "Output folder")
+	outputFolder := flag.String("output", ".", "Output folder")
 	encoderConfig := flag.String("encoder-config", "", "Encoder specific config")
+
 	encoderName := flag.String("encoder", "", "Encoder to be used")
+
 	decoderName := flag.String("decoder", "", "Decoder to be used")
 	decoderConfig := flag.String("decoder-config", "", "Decoder specific config")
+	flag.Parse()
+
+	if *encoderName == "" {
+		fmt.Println("Encoder name can not be empty")
+		os.Exit(1)
+
+	}
 
 	context := generator.Context{
 		EncoderName:   *encoderName,
