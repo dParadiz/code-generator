f2b1a0be05f2b07f596775e5fd3e041c4aa2323f
Dparadiz <dparadiz@gmail.com>
2020-06-04 17:33:00 +0200

    Added base for psr15api decoder

---
+++ internal/plugins/decoder/psr15api/psr15api.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/getkin/kin-openapi/openapi3"
+
+	"github.com/dparadiz/code-generator/internal/generator"
+	"github.com/dparadiz/code-generator/internal/renderer"
+)
+
+type config struct {
+	TemplateFolder string `json:"templateFolder"`
+}
+
+type decoderContext string
+
+func (dc decoderContext) Decode(c *generator.DecoderContext, stack *renderer.Stack) error {
+
+	cfg, err := loadConfig(c.ConfigSource)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("{}", cfg)
+
+	var openapi openapi3.Swagger
+	openapi, ok := c.Data.(openapi3.Swagger)
+	if !ok {
+		return errors.New("Can work only with openapi 3.0 data")
+	}
+
+	fmt.Printf("{}", openapi)
+
+	return nil
+}
+
+func loadConfig(configPath string) (config, error) {
+	jsonFile, err := os.Open(configPath)
+	if err != nil {
+		return config{}, err
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var config config
+	json.Unmarshal(byteValue, &config)
+
+	return config, nil
+}
+
+// Decoder symbol export
+var Decoder decoderContext
